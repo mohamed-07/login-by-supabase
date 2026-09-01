ca8c8019dac1ff8e15aaea80920a7209c263d20f
mohamed <83068325+mohamed-07@users.noreply.github.com>
2026-09-01 11:33:47 +0100

    fis a bug

--- src/routes/Applayout.tsx
+++ src/routes/Applayout.tsx
@@ -1,5 +1,5 @@
-import Navbar from "@/features/layout/Navbar";
-import { Outlet } from "react-router-dom";
+import Navbar from '@/features/layout/Navbar';
+import { Outlet } from 'react-router-dom';
 
 export default function AppLayout() {
   return (
